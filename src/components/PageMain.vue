04c9baf6728ee46b2949fea5174d513e74fea6fb
ChristianMazzaro <christianmazzaro2@gmail.com>
2023-07-25 16:47:28 +0200

    update main sections

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -17,6 +17,7 @@
 <template>
 
     <main>
+      <!-- OFFERS -->
       <section class="what_we_do">
         <div class="section_headers">
           <p>pelicula studio</p>
@@ -73,6 +74,7 @@
 
         </div>
       </section>
+      <!-- ABOUT US -->
       <section class="about_us">
         <div class="text-container">
           <p>About Us</p>
@@ -83,10 +85,166 @@
           </button>
         </div>
       </section>
-      <section></section>
-      <section></section>
-      <section></section>
-      <section></section>
+      <!-- BEST HITS MOVIES -->
+      <section class="what_we_do">
+        <div class="section_headers">
+          <p>YOUR WATCHLIST</p>
+          <h1>BEST HITS MOVIES</h1>
+        </div>
+        <div class="cards_container">
+          <div class="card">
+            <img src="https://picsum.photos/200" alt="">
+            <h1>titolo card</h1>
+            <p>
+              card info
+            </p>
+          </div>
+
+          <div class="card">
+            <img src="https://picsum.photos/200" alt="">
+            <h1>titolo card</h1>
+            <p>
+              card info
+            </p>
+          </div>
+
+          <div class="card">
+            <img src="https://picsum.photos/200" alt="">
+            <h1>titolo card</h1>
+            <p>
+              card info
+            </p>
+          </div>
+
+          <div class="card">
+            <img src="https://picsum.photos/200" alt="">
+            <h1>titolo card</h1>
+            <p>
+              card info
+            </p>
+          </div>
+
+          <div class="card">
+            <img src="https://picsum.photos/200" alt="">
+            <h1>titolo card</h1>
+            <p>
+              card info
+            </p>
+          </div>
+
+          <div class="card">
+            <img src="https://picsum.photos/200" alt="">
+            <h1>titolo card</h1>
+            <p>
+              card info
+            </p>
+          </div>
+
+        </div>
+      </section>
+      <!-- STATS -->
+      <section class="statistic">
+        <div class="stat">
+          <h1>
+            158
+          </h1>
+          <p>
+            satisfied client
+          </p>
+        </div>
+
+        <div class="stat">
+          <h1>
+            158
+          </h1>
+          <p>
+            satisfied client
+          </p>
+        </div>
+
+        <div class="stat">
+          <h1>
+            158
+          </h1>
+          <p>
+            satisfied client
+          </p>
+        </div>
+
+        <div class="stat">
+          <h1>
+            158
+          </h1>
+          <p>
+            satisfied client
+          </p>
+        </div>
+      </section>
+      <!-- OUR TEAM -->
+      <section class="what_we_do">
+        <div class="section_headers">
+          <p>THE PERICUROS</p>
+          <h1>MEET THE TEAM</h1>
+        </div>
+        <div class="cards_container _4_cards">
+          <div class="card">
+            <img src="https://picsum.photos/200" alt="">
+            <h1>titolo card</h1>
+            <p>
+              card info
+            </p>
+          </div>
+
+          <div class="card">
+            <img src="https://picsum.photos/200" alt="">
+            <h1>titolo card</h1>
+            <p>
+              card info
+            </p>
+          </div>
+
+          <div class="card">
+            <img src="https://picsum.photos/200" alt="">
+            <h1>titolo card</h1>
+            <p>
+              card info
+            </p>
+          </div>
+
+          <div class="card">
+            <img src="https://picsum.photos/200" alt="">
+            <h1>titolo card</h1>
+            <p>
+              card info
+            </p>
+          </div>
+
+        </div>
+      </section>
+      <section class="team_carousel">
+        <div class="text-container">
+          <img src="../assets/img/image (20).svg" alt="virgolette">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum voluptate tempora minima consequuntur, omnis voluptas sequi quaerat sint. Sit, animi.
+          </p>
+          <div class="staff_profile">
+            <img src="" alt="immagine profilo">
+            <div class="staff_info">
+              <h1>
+                nome
+              </h1>
+              <p>
+                ruolo
+              </p>
+            </div>
+          </div>
+          <div class="carousel_dots">
+            <span>&bull;</span>
+            <span>&bull;</span>
+            <span>&bull;</span>
+          </div>
+        </div>
+      </section>
       <section></section>
       <section></section>
       <section></section>
@@ -136,38 +294,130 @@
     }
 
     .about_us{
-        background-image: url(../assets/img/About-Us.jpg);
-        background-repeat: no-repeat;
-        background-size: 100% 100%;
-        width: calc(100vw - 40px);
-        padding: 200px;
-        display: flex;
-        justify-content: end;
-        align-items: flex-end;
+      background-image: url(../assets/img/About-Us.jpg);
+      background-repeat: no-repeat;
+      background-size: 100% 100%;
+      width: calc(100vw - 40px);
+      padding: 200px;
+      display: flex;
+      justify-content: end;
+      align-items: flex-end;
 
-        .text-container{
-          color: white;
-          height: 100%;
-          max-width:40%;
+      .text-container{
+        color: white;
+        height: 100%;
+        max-width:40%;
 
-          p{
-            font-size: 22px;
-            margin-bottom: 30px;
-          }
-
-          h1{
-            margin-bottom: 30px;
-            font-size: 35px;
-          }
-
-          button{
-            border: 0;
-            background-color: white;
-            padding: 20px 50px;
-            font-weight: bolder;
-          }
-
+        p{
+          font-size: 22px;
+          margin-bottom: 30px;
         }
+
+        h1{
+          margin-bottom: 30px;
+          font-size: 35px;
+        }
+
+        button{
+          border: 0;
+          background-color: white;
+          padding: 20px 50px;
+          font-weight: bolder;
+        }
+
+      }
     }
+
+    .statistic{
+      background-image: url(../assets/img/Parallax-01.jpg);
+      width: calc(100vw - 40px);
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      padding: 150px;
+      background-size:100%;
+      background-position-y: 500px;
+
+
+
+      .stat{
+        color: white;
+        text-align: center;
+        font-size: 30px;
+
+        h1{
+          font-weight:bolder;
+          font-size: 50px;
+        }
+
+      }
+    }
+    
+    ._4_cards{
+
+      >.card{
+        width: calc(((100% / 4) * 1) - 40px) !important;
+      }
+    }
+
+    .team_carousel{
+      background-image: url(../assets/img/Parallax-02.jpg);
+      width: calc(100vw - 40px);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 150px;
+      background-size:100%;
+      background-position-y:130px;
+      
+
+      .text-container{
+        color: white;
+        height: 100%;
+        max-width:40%;
+
+        >img{
+          filter: invert(1);
+          width: 50px;
+          margin: 20px 330px;
+        }
+
+        p{
+          text-align: center;
+          font-size: 18px;
+          line-height: 35px;
+        }
+
+        h1{
+          font-size: 22px;
+        }
+
+        .staff_profile{
+          display: flex;
+          text-align: center;
+          justify-content: center;
+          align-items: center;
+          margin: 10px;
+
+          .staff_info{
+            display:flex ;
+            flex-direction: column;
+            margin: 0 10px;
+          }
+        }
+
+        .carousel_dots{
+          text-align: center;
+          font-size: 40px;
+
+          :nth-child(2){
+            margin: 0 10px;
+          }
+        }
+
+      }
+
+    }
+
   }
 </style>
